9febca22f79c20302008f435b3a826810ae6a43b
tainuth1 <tainuth1@gmail.com>
2025-04-21 13:38:42 +0700

    show cart item in checkout, and remove function

---
+++ src/javascript/checkout.js
@@ -0,0 +1,82 @@
+let carts = JSON.parse(localStorage.getItem("carts")) || [];
+
+const cartContainer = document.getElementById("cart-item-container");
+
+const renderCartItem = () => {
+  let cartLists = "";
+
+  carts.forEach((cart) => {
+    cartLists += `
+        <div class="grid grid-cols-12 py-4 items-center border-b-2">
+            <div class="col-span-5 text-gray-700 flex gap-4">
+                <img
+                class="w-16 border aspect-square object-cover p-1 rounded-md"
+                src="${cart.image}"
+                alt="Product Image"
+                />
+                <div class="">
+                <h2 class="w-[220px] truncate font-medium">
+                    ${cart.name}
+                </h2>
+                <span class="font-medium text-gray-800"
+                    >Category :
+                    <span class="text-gray-600">${cart.category}</span></span
+                >
+                <p class="font-medium text-gray-800"
+                    >Price :
+                    <span class="text-gray-600">$${cart.price}</span>
+                </p>
+                </div>
+            </div>
+            <div class="col-span-3 text-gray-700">
+                <div
+                class="inline space-x-2 border border-gray-300 rounded-full px-4 py-1 shadow-sm"
+                >
+                <button
+                    class="text-lg font-medium px-1 text-gray-700 hover:text-red-500"
+                >
+                    -
+                </button>
+                <span class="text-lg font-medium">${cart.quantity}</span>
+                <button
+                    class="text-lg font-medium px-1 text-gray-700 hover:text-green-500"
+                >
+                    +
+                </button>
+                </div>
+            </div>
+            <div class="col-span-3 text-gray-700 font-bold">$${
+              cart.price * cart.quantity
+            }</div>
+            <div class="col-span-1 text-gray-700">
+            <button onclick="removeCartItem(${
+              cart.id
+            })" class="hover:text-red-500">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="w-6 h-6"
+                >
+                    <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M6 18 18 6M6 6l12 12"
+                    />
+                </svg>
+                </button>
+            </div>
+        </div>
+    `;
+  });
+  cartContainer.innerHTML = cartLists;
+};
+renderCartItem();
+
+const removeCartItem = (id) => {
+  carts = carts.filter((pro) => pro.id != id);
+  localStorage.setItem("carts", JSON.stringify(carts));
+  renderCartItem();
+};
